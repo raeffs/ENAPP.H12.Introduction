2c8a25f1d4c5aee8f53aaa46fedea1fee136790a
Raphael Fleischlin <git@raefftec.ch>
2012-10-04 10:31:25 +0200

    Tutorial 6

--- EatMe/EatMe-ejb/src/java/eatme/boundary/WeatherManager.java
+++ EatMe/EatMe-ejb/src/java/eatme/boundary/WeatherManager.java
@@ -1,8 +1,18 @@
 package eatme.boundary;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+import eatme.boundary.dto.Cloudiness;
+import eatme.boundary.dto.ProductType;
+import eatme.boundary.dto.TimeType;
 import eatme.boundary.dto.WeatherResponseFromString;
+import eatme.boundary.dto.Weatherdata;
 import eatme.boundary.webservices.weather.GlobalWeather;
 import java.io.StringReader;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateful;
 import javax.xml.bind.JAXBContext;
@@ -21,7 +31,7 @@ public class WeatherManager {
 
     public String getTodaysWeather() {
         if (todaysWeather == null || (System.currentTimeMillis() - lastUpdate) > (1000 * 3600 * 12)) {
-            callWeatherService();
+            callWeatherServiceRestful();
         }
         return todaysWeather;
     }
@@ -45,6 +55,43 @@ public class WeatherManager {
             response = (WeatherResponseFromString) unmarshaller.unmarshal(new StringReader(rawResponse));
         } finally {
             return response;
+        }
+    }
+
+    private void callWeatherServiceRestful() {
+        String base_uri = "http://api.met.no/weatherapi/locationforecast";
+
+        ClientConfig config = new DefaultClientConfig();
+        Client client = Client.create(config);
+        WebResource webResource = client.resource(base_uri).path("1.8");
+
+        Weatherdata result = webResource
+                .queryParam("lat", "47.0137")
+                .queryParam("lon", "8.3054")
+                .queryParam("msl", "440")
+                .get(Weatherdata.class);
+        Cloudiness c = null;
+        for (ProductType pt : result.getProduct()) {
+            Calendar gc = new GregorianCalendar();
+            for (TimeType t : pt.getTime()) {
+                if (t.getFrom().getDay() == gc.get(Calendar.DAY_OF_MONTH)
+                        && t.getFrom().getHour() == 12) {
+                    try {
+                        c = (Cloudiness) t.getLocation().get(0)
+                                .getGroundCoverAndPressureAndMaximumPrecipitation()
+                                .get(5)
+                                .getValue();
+                        break;
+                    } catch (Exception e) {
+                        // nothing to do
+                    }
+                }
+            }
+        }
+        if (c == null) {
+            this.todaysWeather = "unknown";
+        } else {
+            this.todaysWeather = c.getPercent();
         }
     }
 }
